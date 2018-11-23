b1cf1686f7f05a665b081c6a7a3ccba6319459e5
Nícolas Wilson Souza <nwsouza66@gmail.com>
2018-11-23 21:00:10 -0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
+#include <semphr.h>
+
+#define qtyFilo 10
+
+SemaphoreHandle_t xGarfosSem[qtyFilo];
+SemaphoreHandle_t xSerial;
+
+void TaskFilosofos(void *pvParameters);
+/*void TaskFilosofos2(void *pvParameters);
+void TaskFilosofos3(void *pvParameters);
+void TaskFilosofos4(void *pvParameters);
+void TaskFilosofos5(void *pvParameters);*/
+
+void setup()
+{
+  Serial.begin(9600);
+
+  for (int x = 0; x < qtyFilo; x++)
+  {
+    xGarfosSem[x] = xSemaphoreCreateMutex();
+  }
+  xSerial = xSemaphoreCreateMutex();
+  for (int x = 0; x < 10; x++)
+  {
+    xTaskCreate(TaskFilosofos, (const portCHAR *)"Filosofos", 128, (void *)x, 1, NULL);
+  }
+}
+void loop()
+{
+}
+
+void TaskFilosofos(void *parameter)
+{
+  int x, x2;
+  x = (int)parameter;
+
+  if (x == (qtyFilo - 1))
+  {
+    x2 = 0;
+  }
+  else
+    x2 = x + 1;
+
+  //(void)pvParameters;
+  while (1)
+  {
+    if (xSemaphoreTake(xGarfosSem[x], (TickType_t)0) == pdTRUE)
+    {
+      if (xSemaphoreTake(xGarfosSem[x2], (TickType_t)0) == pdTRUE)
+      {
+        if (xSemaphoreTake(xSerial, (TickType_t)portMAX_DELAY) == pdTRUE)
+        {
+          Serial.print("Filosofo ");
+          Serial.print(x);
+          Serial.println(" esta comendo///////////////////////////////");
+          xSemaphoreGive(xSerial);
+        }
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // wait for one second
+        //vTaskDelay(random(1000, 10000) / portTICK_PERIOD_MS);
+        xSemaphoreGive(xGarfosSem[x]);
+        xSemaphoreGive(xGarfosSem[x2]);
+        
+      }
+      else
+        //xSemaphoreGive(xGarfosSem[x2]);
+        xSemaphoreGive(xGarfosSem[x]);
+    }
+    //else
+    //xSemaphoreGive(xGarfosSem[x]);
+    if (xSemaphoreTake(xSerial, (TickType_t)portMAX_DELAY) == pdTRUE)
+    {
+      Serial.print("Filósofo ");
+      Serial.print(x);
+      Serial.println(" esta pensando");
+      xSemaphoreGive(xSerial);
+    }
+    vTaskDelay(2000 / portTICK_PERIOD_MS); // wait for one second
+  }
+}
